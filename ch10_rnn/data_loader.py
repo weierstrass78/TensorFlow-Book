b259c40fa562d7d3413ae64dab4dfdb59f9be029
Nishant Shukla <nick722@gmail.com>
2017-03-06 14:28:09 -0800

    updated chapter 10 code to TensorFlow v1.0

--- ch10_rnn/data_loader.py
+++ ch10_rnn/data_loader.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 def load_series(filename, series_idx=1):
     try:
-        with open(filename, 'rb') as csvfile:
+        with open(filename) as csvfile:
             csvreader = csv.reader(csvfile)
             data = [float(row[series_idx]) for row in csvreader if len(row) > 0]
             normalized_data = (data - np.mean(data)) / np.std(data)
