163c15e020d327f6fb81f0f7d668630a2a0e0ad4
Nishant Shukla <nick722@gmail.com>
2017-05-02 14:13:53 -0700

    fixed bug in data reader

--- ch03_regression/data_reader.py
+++ ch03_regression/data_reader.py
@@ -1,14 +1,13 @@
 import csv
 import time
 
-# https://data.lacity.org/dataset/311-Call-Center-Tracking-Data/ukiu-8trj
+# 
 # crime14_freq = data_reader.read('crimes_2014.csv', 1, '%d-%b-%y %H:%M:%S', 2014)
 # freq = read('311.csv', 0, '%m/%d/%Y', 2014)
 
 def read(filename, date_idx, date_parse, year, bucket=7):
 
     days_in_year = 365
-    bucket = 7
 
     # Create initial frequency map
     freq = {}
@@ -20,8 +19,14 @@ def read(filename, date_idx, date_parse, year, bucket=7):
         csvreader = csv.reader(csvfile)
         csvreader.next()
         for row in csvreader:
+            if row[date_idx] == '':
+                continue
             t = time.strptime(row[date_idx], date_parse)
             if t.tm_year == year and t.tm_yday < (days_in_year-1):
                 freq[int(t.tm_yday / bucket)] += 1
 
     return freq
+
+if __name__ == '__main__':
+    freq = read('311.csv', 0, '%m/%d/%Y', 2014)
+    print freq
